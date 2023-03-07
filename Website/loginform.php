670785d20b40a8d3fd6c2ee094a89fb644239007
Richard Marcell <richard.marcell8888@gmail.com>
2023-03-07 10:43:28 +0700

    membuat tampilan login user dan membuatnya responsif

--- Website/loginform.php
+++ Website/loginform.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Style/stylesignup.css">
+    <link rel="stylesheet" href="../Style/stylelogin.css">
     <title>Toko Ajun | Login Form</title>
 </head>
 <body>
@@ -31,44 +31,10 @@
 
                     <div class="button">
                         <button type="submit" id="login1" name="login">Login</button>
-                        <button id="login2">Sign Up</button>
+                        <a href="signupform.php" id="login2">Sign Up</a>
                     </div>
                 </form>
             </div>
         </div>
-        <div class="kanan">
-            <div class="logo">
-                <img src="../assets//img/logo vertikal.svg" alt="">
-            </div>
-            <div class="opening">
-                <h1>Hello New User!!</h1>
-                <h3>You must have an account join us</h3>
-            </div>
-            <div class="formSignUp">
-                <form action="">
-                    <label for="username">Username</label>
-                    <input type="text" placeholder="example: rcmfrey7673" name="username" id="username"><br>
-
-                    <label for="email">Email</label>
-                    <input type="email" placeholder="example: richard.marcell@gmail.com" name="email" id="email"><br>
-
-                    <label for="password">Password</label>
-                    <input type="password" placeholder="input your password account" id="password" name="password"><br>
-
-                    <div class="button">
-                        <button id="signUp1">Login</button>
-                        <button type="submit" id="signUp2" name="signUp">Sign Up</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <script>
-            let login = document.getElementById('login2');
-            let animasi = document.getElementById('animasi');
-            login.addEventListener('click', () => {
-                animasi.style.transform ="translateX(-100%)";
-            })
-        </script>
 </body>
 </html>
